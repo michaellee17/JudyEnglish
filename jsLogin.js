cf993f4ddb388e441307e0db483ac2ee27da8933
michael17 <missing821017@yahoo.com.tw>
2022-04-25 18:00:42 +0800

    4.25.2 commit

--- jsLogin.js
+++ jsLogin.js
@@ -1,10 +1,10 @@
 window.addEventListener("scroll", function() {
     let header = document.getElementById("logo");
-    if (document.documentElement.scrollTop >= 400) {
+    if (document.documentElement.scrollTop >= 300) {
         header.classList.add("smallPanel");
 
 
-    } else {
+    } else if (document.documentElement.scrollTop <= 200) {
         header.classList.remove("smallPanel");
     }
 })
